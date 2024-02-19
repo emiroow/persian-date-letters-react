69acfee9e03f252279f488e4bbc3d60e41db84c6
emiroow <amir_esmaeelzadeh@yahoo.com>
2024-02-20 00:20:09 +0330

    fix layout

--- src/template/Layout.tsx
+++ src/template/Layout.tsx
@@ -7,68 +7,19 @@ interface Props {
 const Layout: FC<Props> = ({ children }) => {
   return (
     <div className="drawer">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
         {/* Navbar */}
-        <div className="w-full navbar bg-base-300 p-5">
-          <div className="flex-none lg:hidden">
-            <label
-              htmlFor="my-drawer-3"
-              aria-label="open sidebar"
-              className="btn btn-square btn-ghost"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-6 h-6 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
-            </label>
-          </div>
-          <div className="flex-1 px-2 mx-2 font-bold text-xl">
+        <div className="w-full max-lg:hidden navbar bg-base-300 p-5">
+          <div className="flex-1 p-2 mx-2 font-bold text-xl">
             Persian date to letters
           </div>
-          <div className="flex-none hidden lg:block">
-            <ul className="menu menu-horizontal">
-              {/* Navbar menu content here */}
-              <li>
-                <a>Navbar Item 1</a>
-              </li>
-              <li>
-                <a>Navbar Item 2</a>
-              </li>
-            </ul>
-          </div>
+          <span className="p-2 mx-2">v 1.0.0</span>
         </div>
         {/* Page content here */}
         <>{children}</>
         <div className="w-full text-center mt-5 mb-8">
           <span className="text-sm ">Good Luck 👋</span>
         </div>
-      </div>
-
-      <div className="drawer-side">
-        <label
-          htmlFor="my-drawer-3"
-          aria-label="close sidebar"
-          className="drawer-overlay"
-        ></label>
-        <ul className="menu p-4 w-80 min-h-full bg-base-200">
-          {/* Sidebar content here */}
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
-        </ul>
       </div>
     </div>
   );
